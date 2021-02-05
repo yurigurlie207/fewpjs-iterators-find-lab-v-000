9dcf12279b2b89228c3f5a70a5384e425ae6954d
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-05 18:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
